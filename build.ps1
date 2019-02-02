55f17c9c39e62248260bc53233f3a70ca936effe
Brett Miller <brett@millerb.co.uk>
2019-02-02 16:59:24 +0000

    Dev (#8)
    * Updated Readme with new functionality
    
    * updated img paths
    
    * Moved img folder into RDCManager sub-folder
    
    * Moved img folder into RDCManager sub-folder
    
    * added spaces for psm1 compilation
    
    * Added FunctionsToExport manually
    
    * Added build script for AzurePipelines
    
    * Updated Build Path Error
    
    * Updated README for img path change
    
    * Updated Paths
    
    * Updated Paths

--- build.ps1
+++ build.ps1
@@ -13,24 +13,24 @@ function Get-FilesToMerge {
     Get-ChildItem -Path $PSScriptRoot -Recurse -Include *.ps1 -Exclude build.ps1 |
         Sort-Object FullName | ForEach-Object {
             "Adding {0} to the psm1 file" -f $_.BaseName | Write-Verbose
-            Get-Content -Path $_.FullName | Add-Content -Path $Destination\RDCManager\RDCManager.psm1
+            Get-Content -Path $_.FullName | Add-Content -Path $PSScriptRoot\out\RDCManager\RDCManager.psm1
             "`r" | Add-Content -Path $Destination\RDCManager\RDCManager.psm1
         }
 }
 
+# Remove out directory to allow rebuilding
 if (Test-Path -Path $PSScriptRoot\out) {
     Remove-Item -Path $Destination -Force -Confirm:$false -Recurse
 }
 
-# Remove out directory to allow rebuilding
 $null = New-Item -ItemType Directory -Name RDCManager -Path $PSScriptRoot\out
 
 Get-FilesToMerge
 
 "Adding InitializeModule to the psm1 file" | Write-Verbose
-"InitializeModule" | Add-Content -Path $Destination\RDCManager\RDCManager.psm1
+"InitializeModule" | Add-Content -Path $PSScriptRoot\out\RDCManager\RDCManager.psm1
 
 "Copying Manifest file to {0}" -f $Destination | Write-Verbose
-Copy-Item -Path $PSScriptRoot\RDCManager\*.psd1 -Destination $Destination\RDCManager
+Copy-Item -Path $PSScriptRoot\RDCManager\*.psd1 -Destination $PSScriptRoot\out\RDCManager
 
 #$content -replace "^FunctionsToExport = '[*]'$", ("FunctionsToExport = @('{0}'`r`n)" -f ($pubfunctions -join "',`r`n`t'"))
